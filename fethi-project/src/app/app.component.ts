a22bf0c910925956e2c0758e81baeb322bcaf444
LEGALOUDECFlorian <pti.breton@hotmail.fr>
2025-03-20 09:09:27 +0100

    feat: joke for fethi

--- fethi-project/src/app/app.component.ts
+++ fethi-project/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapCaseComponent } from './components/map-case.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapCaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fethi-project';
+  title = 'Where is Fethi ?';
 }
---
+++ fethi-project/src/app/components/map-case.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-case',
+  imports: [],
+  template: `
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5255.181766306272!2d2.3339226999999996!3d48.808786399999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6710e6372ab91%3A0xa3b18d74ac5e2ca8!2sMaison%20des%20Examens%20-%20SIEC!5e0!3m2!1sfr!2sfr!4v1742374976866!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  `,
+  styles: ``
+})
+export class MapCaseComponent {
+
+}
